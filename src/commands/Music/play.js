1e250cd0f0898a8dc9da34214e4dcf87a10a9d2d
Mauro De Rosa <it.mauro.derosa@gmail.com>
2024-09-17 18:21:41 +0200

    created basic sapphire bot with ping command and a test of play command with string option

---
+++ src/commands/Music/play.js
@@ -0,0 +1,41 @@
+const { isMessageInstance } = require('@sapphire/discord.js-utilities');
+const { Command } = require('@sapphire/framework');
+
+class MusicPlayCommand extends Command {
+
+    constructor(context, options) {
+        super(context, { ...options, fullCategory: ['Music'] });
+    }
+
+    registerApplicationCommands(registry) {
+        registry.registerChatInputCommand((builder) =>
+            builder.setName('play')
+                .setDescription('Play the requested youtube link or youtube title')
+                .addStringOption(option =>
+                    option.setName('link-or-query')
+                        .setDescription('youtube link or video\'s name')
+                        .setRequired(true)
+                )
+        );
+    }
+
+    async chatInputRun(interaction) {
+        const linkOrQuery = interaction.options.getString('link-or-query');
+
+        const msg = await interaction.reply({
+            content: `Did you want to download: \'${linkOrQuery}\' right?`,
+            ephemeral: true,
+            fetchReply: true
+        });
+
+        if (isMessageInstance(msg)) {
+            return interaction.editReply(`Eh! you wanted to download ${linkOrQuery}, look at that face! look at that face, he didn't expect it`);
+        }
+
+        return interaction.editReply('Failed to retrieve ping TwT');
+    }
+
+}
+
+module.exports = { MusicPlayCommand }
+
---
+++ src/index.js
@@ -0,0 +1,10 @@
+const { SapphireClient } = require('@sapphire/framework');
+const { GatewayIntentBits } = require('discord.js');
+require('dotenv').config()
+
+const client = new SapphireClient({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+    defaultPrefix: process.env.BOT_PREFIX
+});
+
+client.login(process.env.BOT_TOKEN);
